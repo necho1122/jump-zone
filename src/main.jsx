501b62ce1c69c0d19baf6e001816b4042d07c199
necho1122 <nelsonubac90@gmail.com>
2024-06-15 18:40:09 -0300

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     clientId="IJMXcKDAU03DBF3pnoxJ1E7r3CM9HASn"
     cacheLocation="memory"
     authorizationParams={{
-      redirect_uri: 'http://localhost:5173/samplicio'
+      redirect_uri: 'https://jump-zone-app.vercel.app'
     }}
   >
     <BrowserRouter>
